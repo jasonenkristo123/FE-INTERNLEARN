a5da63a4d03804ee48c42578643e5f23c7b5c4a9
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-05 09:01:23 +0700

    finished learning zod(theory)

---
+++ app/lib/validate.ts
@@ -0,0 +1,38 @@
+import Error from "next/error";
+import { z } from "zod";
+
+
+type User = z.infer<typeof userSchema>
+
+const hobbies = ["Programming", "Weight lifting", "Reading"] as const
+
+const userSchema = z.object({
+    id: z.union([z.string(), z.number()]), // artinya id bisa antara string atau number
+    username: z.string(),
+    age: z.number().gt(0),
+    email: z.email(),
+    birthday: z.date(),
+    hobbies: z.enum(hobbies),
+    friends: z.array(z.string()).nonempty(),
+    coordinates: z.tuple([z.number(), z.number(), z.number().gt(4).int()])
+}).strict()
+
+const user: User = {
+    id: 1,
+    username: "JasZ",
+    age: 21,
+    email: "jasz@gmail.com",
+    birthday: new Date(),
+    hobbies: "Programming",
+    friends: ["Jason", "Habibi", "Arfa"],
+    coordinates: [1, 2, 40]
+}
+
+console.log(userSchema.parse(user))
+
+const UserSchema2 = z.object({
+    id: z.discriminatedUnion("status", [
+        z.object({ status: z.literal("success"), data: z.string()}),
+        z.object({ status: z.literal("error"), error: z.instanceof(Error)})
+    ])
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
     </main>
   )
-
 }
