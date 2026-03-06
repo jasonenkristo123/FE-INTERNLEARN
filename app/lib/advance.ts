6254b1904693c5bf17258b97df6fba6cd285e640
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-06 22:27:34 +0700

    learn typescript advance

---
+++ app/lib/advance.ts
@@ -0,0 +1,37 @@
+type user = {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    age: number;
+}
+
+type updateUser = Partial<user>; // mengubah semua field menjadi optional
+
+interface username {
+    name?: string;
+    age?: number;
+}
+
+type RequiredUsername = Required<username>;
+
+type UserLogin = {
+    email: string;
+    password: string;
+    number: number;
+}
+
+type emailUser = Pick<UserLogin, "email">; // hanya memilih email
+
+type RemoveNumber = Omit<UserLogin, "number">; // remove
+
+type role = "admin" | "user" | "guest";
+
+type Permisi = Record<role, boolean>;
+
+const permissions: Permisi = {
+    admin: true,
+    user: false,
+    guest: false
+}
+
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,88 +1,104 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import { LoginSchema, LoginType } from "../schema/auth.schema";
+import { useState } from 'react'
+import { LoginSchema, LoginType } from '../schema/auth.schema'
 
 export default function Login() {
-    // 1. Setup states for our form inputs and validation errors
-    const [formData, setFormData] = useState<LoginType>({email: "", password: ""});
-    const [errors, setErrors] = useState<{email?: string; password?: string;}>({});
+  // 1. Setup states for our form inputs and validation errors
+  const [formData, setFormData] = useState<LoginType>({
+    email: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {},
+  )
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value
-        })
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
 
-        if (errors[e.target.id as keyof typeof errors]) {
-            setErrors({ ...errors, [e.target.id]: undefined})
-        }
+    if (errors[e.target.id as keyof typeof errors]) {
+      setErrors({ ...errors, [e.target.id]: undefined })
+    }
+  }
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const result = LoginSchema.safeParse(formData)
+
+    if (!result.success) {
+      const fieldErrors = result.error.flatten().fieldErrors
+
+      setErrors({
+        email: fieldErrors.email?.[0],
+        password: fieldErrors.password?.[0],
+      })
+      return
     }
 
-    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    setErrors({})
+    console.log('valid', result.data)
+  }
 
-        const result = LoginSchema.safeParse(formData);
+  return (
+    <main className="w-full min-h-screen bg-slate-950 flex items-center justify-center">
+      <section className="sm:w-lg bg-slate-100 rounded-lg p-8">
+        {/* 6. Attach the handleSubmit function to the form */}
+        <form onSubmit={handleSubmit}>
+          <h1 className="text-center text-2xl font-medium pt-5">Login</h1>
 
-        if (!result.success) {
-            const fieldErrors = result.error.flatten().fieldErrors;
+          <div className="pt-5">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter Email"
+              className={`w-full border rounded-md p-2 outline-none ${errors.email ? 'border-red-500' : 'border-slate-300 focus:border-blue-500'}`}
+            />
+            {/* 7. Display the email error message if it exists */}
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
+          </div>
 
-            setErrors({
-                email: fieldErrors.email?.[0],
-                password: fieldErrors.password?.[0]
-            });
-            return;
-        }
+          <div className="pt-5">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+              className={`w-full border rounded-md p-2 outline-none ${errors.password ? 'border-red-500' : 'border-slate-300 focus:border-blue-500'}`}
+            />
+            {/* 8. Display the password error message if it exists */}
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
+          </div>
 
-        setErrors({});
-        console.log("valid", result.data);
-    }
+          <div className="mt-10 text-center w-full bg-blue-950 py-3 text-white rounded-md hover:bg-blue-900 transition-colors">
+            <button type="submit" className="w-full">
+              SIGN IN
+            </button>
+          </div>
 
-    return (
-        <main className="w-full min-h-screen bg-slate-950 flex items-center justify-center">
-            <section className="sm:w-lg bg-slate-100 rounded-lg p-8">
-                {/* 6. Attach the handleSubmit function to the form */}
-                <form onSubmit={handleSubmit}>
-                    <h1 className="text-center text-2xl font-medium pt-5">Login</h1>
-
-                    <div className="pt-5">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder="Enter Email"
-                            className={`w-full border rounded-md p-2 outline-none ${errors.email ? 'border-red-500' : 'border-slate-300 focus:border-blue-500'}`}
-                        />
-                        {/* 7. Display the email error message if it exists */}
-                        {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
-                    </div>
-
-                    <div className="pt-5">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            placeholder="Enter your password"
-                            className={`w-full border rounded-md p-2 outline-none ${errors.password ? 'border-red-500' : 'border-slate-300 focus:border-blue-500'}`}
-                        />
-                        {/* 8. Display the password error message if it exists */}
-                        {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
-                    </div>
-
-                    <div className="mt-10 text-center w-full bg-blue-950 py-3 text-white rounded-md hover:bg-blue-900 transition-colors">
-                        <button type="submit" className="w-full">SIGN IN</button>
-                    </div>
-
-                    <div className="mt-10 mb-5 text-center text-sm">
-                        <p className="pb-2">Forgot Username/Password?</p>
-                        <p>Don&apos;t have an account? <a href="#" className="text-blue-600 font-medium">Sign Up</a></p>
-                    </div>
-                </form>
-            </section>
-        </main>
-    );
+          <div className="mt-10 mb-5 text-center text-sm">
+            <p className="pb-2">Forgot Username/Password?</p>
+            <p>
+              Don&apos;t have an account?{' '}
+              <a href="#" className="text-blue-600 font-medium">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </form>
+      </section>
+    </main>
+  )
 }
