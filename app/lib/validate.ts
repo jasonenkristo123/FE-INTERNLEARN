45892e9667f1b56d212a916452c366969758c100
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-06 22:10:43 +0700

    learn zod with implementation

--- app/lib/validate.ts
+++ app/lib/validate.ts
@@ -1,12 +1,12 @@
-import Error from "next/error";
-import { z } from "zod";
-
+import Error from 'next/error'
+import { z } from 'zod'
 
 type User = z.infer<typeof userSchema>
 
-const hobbies = ["Programming", "Weight lifting", "Reading"] as const
+const hobbies = ['Programming', 'Weight lifting', 'Reading'] as const
 
-const userSchema = z.object({
+const userSchema = z
+  .object({
     id: z.union([z.string(), z.number()]), // artinya id bisa antara string atau number
     username: z.string(),
     age: z.number().gt(0),
@@ -14,25 +14,26 @@ const userSchema = z.object({
     birthday: z.date(),
     hobbies: z.enum(hobbies),
     friends: z.array(z.string()).nonempty(),
-    coordinates: z.tuple([z.number(), z.number(), z.number().gt(4).int()])
-}).strict()
+    coordinates: z.tuple([z.number(), z.number(), z.number().gt(4).int()]),
+  })
+  .strict()
 
 const user: User = {
-    id: 1,
-    username: "JasZ",
-    age: 21,
-    email: "jasz@gmail.com",
-    birthday: new Date(),
-    hobbies: "Programming",
-    friends: ["Jason", "Habibi", "Arfa"],
-    coordinates: [1, 2, 40]
+  id: 1,
+  username: 'JasZ',
+  age: 21,
+  email: 'jasz@gmail.com',
+  birthday: new Date(),
+  hobbies: 'Programming',
+  friends: ['Jason', 'Habibi', 'Arfa'],
+  coordinates: [1, 2, 40],
 }
 
 console.log(userSchema.parse(user))
 
 const UserSchema2 = z.object({
-    id: z.discriminatedUnion("status", [
-        z.object({ status: z.literal("success"), data: z.string()}),
-        z.object({ status: z.literal("error"), error: z.instanceof(Error)})
-    ])
+  id: z.discriminatedUnion('status', [
+    z.object({ status: z.literal('success'), data: z.string() }),
+    z.object({ status: z.literal('error'), error: z.instanceof(Error) }),
+  ]),
 })
---
+++ app/schema/auth.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const LoginSchema = z.object({
+    email: z.string().email("Email tidak valid"),
+    password: z.string().min(8, "Password minimal 8 karakter"),
+})
+
+export type LoginType = z.infer<typeof LoginSchema>
